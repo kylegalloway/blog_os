7bec500aa2a1cb518a5bac4761497ac8282146fa
Kyle Galloway <kyle.galloway.1993@gmail.com>
2018-03-26 12:06:24 -0500

    Up through 'VGA Text Mode'

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 #![feature(lang_items)] // required for defining the panic handler
+#![feature(const_fn)] // allows for functions to be marked as const and called in const scopes
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
 extern crate rlibc;
+extern crate spin;
+extern crate volatile;
+
+#[macro_use]
+extern crate lazy_static;
+
+#[macro_use]
+mod vga_buffer;
 
 #[lang = "panic_fmt"] // define the function that should be called on panic
 #[no_mangle]
@@ -15,19 +24,9 @@ pub extern "C" fn rust_begin_panic(
     loop {}
 }
 
-static OUTPUT: &[u8] = b"Leeroy nnnJenkins!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *const u8 as *mut u8; // cast 0xb8000 into a raw pointer
-
-    for (i, &byte) in OUTPUT.iter().enumerate() {
-        // iterate over OUTPUT
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte; // write the string byte
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // write the colorbyte (0xb is light cyan)
-        }
-    }
+    println!("Leeroy {}Jenkins!", "nnnnnn");
 
     loop {}
 }
