48840b0f634b47d61310a769161cdfe62ca6a3de
Kyle Galloway <kyle.galloway.1993@gmail.com>
2018-05-02 15:39:34 -0500

    Through Unit Testing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 #![feature(lang_items)] // required for defining the panic handler
 #![feature(const_fn)] // allows for functions to be marked as const and called in const scopes
 #![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![cfg_attr(not(test), no_main)] // disable all Rust-level entry points if not testing
+#![cfg_attr(test, allow(dead_code, unused_macros))] // Silence the warnings when testing
+
+#[cfg(test)] // Only import if testing
+extern crate std;
+
+#[cfg(test)]
+extern crate array_init;
 
 extern crate rlibc;
 extern crate spin;
@@ -13,6 +20,7 @@ extern crate lazy_static;
 #[macro_use]
 mod vga_buffer;
 
+#[cfg(not(test))] // Only compile when the test flag is not set
 #[lang = "panic_fmt"] // define the function that should be called on panic
 #[no_mangle]
 pub extern "C" fn rust_begin_panic(
@@ -24,6 +32,7 @@ pub extern "C" fn rust_begin_panic(
     loop {}
 }
 
+#[cfg(not(test))] // Only compile when the test flag is not set
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Leeroy {}Jenkins!", "nnnnnn");
