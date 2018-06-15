32df6618fcb83d2937b770f0582125f146db2709
Kyle Galloway <kyle.galloway.1993@gmail.com>
2018-06-15 08:54:30 -0500

    Through integrations tests

--- src/bin/test-basic-boot.rs
+++ src/bin/test-basic-boot.rs
@@ -1,11 +1,14 @@
 #![feature(panic_implementation)] // required for defining the panic handler
+#![feature(const_fn)]
 #![no_std] // don't link the Rust standard library
-#![cfg_attr(not(test), no_main)] // disable all Rust-level entry points if not testing
-#![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))] // Silence the warnings when testing
+#![cfg_attr(not(test), no_main)] // disable all Rust-level entry points
+#![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
 
+// add the library as dependency (same crate name as executable)
 #[macro_use]
 extern crate blog_os;
 
+use blog_os::exit_qemu;
 use core::panic::PanicInfo;
 
 /// This function is the entry point, since the linker looks for a function
@@ -13,15 +16,25 @@ use core::panic::PanicInfo;
 #[cfg(not(test))]
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Leeroy {}Jenkins!", "nnnnnn");
+    serial_println!("ok");
 
+    unsafe {
+        exit_qemu();
+    }
     loop {}
 }
 
+/// This function is called on panic.
 #[cfg(not(test))]
 #[panic_implementation]
 #[no_mangle]
 pub fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
+    serial_println!("failed");
+
+    serial_println!("{}", info);
+
+    unsafe {
+        exit_qemu();
+    }
     loop {}
 }
